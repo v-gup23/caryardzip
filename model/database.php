4eea21544fc35e90b402366a1249dbbc287d3b80
v-gup23 <143101323+v-gup23@users.noreply.github.com>
2024-03-21 12:22:05 +0300

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,13 +1,15 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-$username = 'root';
-$password = '';
+    $dsn = 'mysql:vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; port=3306; dbname=x6xvxij8stbkvt96';
+    $username = 'taz01w7c42lszkvs';
+    $password = 'amqc2qcbgy5ly5sf';
 
 try {
     $db = new PDO($dsn, $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    $error_message = $e->getMessage();
+    $error = "Database Error: ";
+    $error .= $e->getMessage();
+    $error .= " (Error Code: " . $e->getCode() . ", SQLSTATE: " . $e->errorInfo[0] . ")";
     include('view/error.php');
     exit();
 }
